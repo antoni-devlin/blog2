9f34ffa9ef5429184bcede6c674bbfaaeac35e03
antoni-devlin <antoni.devlin@gmail.com>
2018-08-27 00:20:24 +0100

    Regenerated database. Removed first_name, last_name fields. Fixed slug generation, added CKEditor.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, url_for, render_template, request, flash, redirect
 from slugify import slugify
-from flask_sqlalchemy import SQLAlchemy, event
+from flask_sqlalchemy import *
 from flask_migrate import Migrate
+from flask_login import LoginManager, UserMixin, logout_user, login_user, current_user, login_required, login_manager
 from datetime import datetime
-from forms import *
+from forms import AddEditPost, LoginForm, RegistrationForm
+from flask_ckeditor import CKEditor
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.urls import url_parse
 import os
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
@@ -14,8 +18,10 @@ app.config['SECRET_KEY'] = 'sjshlaiyeiruhkjgavksnlkvnslvsnlvsnlvnsdh536574988tuf
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+ckeditor = CKEditor(app)
 
 class Post(db.Model):
 
@@ -31,25 +37,52 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
 # Automatic Slug generation (using slugify)
     @staticmethod
     def generate_slug(target, value, oldvalue, initiator):
-        if value and (not target.slug or value  != oldvalue):
+        if not target.slug:
             target.slug = slugify(value)
 
 event.listen(Post.title, 'set', Post.generate_slug, retval=False)
 
+class User(UserMixin, db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 #Home Page Route
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
 @app.route('/')
 @app.route('/index')
 def index():
-
     title = 'Antoni Devlin | Blog'
     posts = Post.query.all()
     return render_template('index.html', posts=posts, title=title)
 
 #Add New Post Route
 @app.route('/add', methods=['GET', 'POST'])
+@login_required
 def add_post():
     form = AddEditPost()
     if form.validate_on_submit():
@@ -61,6 +94,7 @@ def add_post():
 
 # Edit Existing Post
 @app.route('/edit/<string:slug>', methods=['GET', 'POST'])
+@login_required
 def edit_post(slug):
     post = Post.query.filter_by(slug=slug).first_or_404()
     form = AddEditPost(obj=post)
@@ -71,9 +105,10 @@ def edit_post(slug):
         post.body = form.body.data
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('add.html', form=form)
+    return render_template('add.html', form=form, title='Edit Post')
 
 @app.route('/delete/<string:slug>')
+@login_required
 def delete_post(slug):
     Post.query.filter_by(slug=slug).delete()
     db.session.commit()
@@ -86,11 +121,42 @@ def byslug(slug):
     post = Post.query.filter_by(slug=slug).first_or_404()
     return render_template("post.html", post=post, slug=slug)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    title = 'Login'
-    return render_template('login.html', title=title)
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/contact')
 def contact():
--- forms.py
+++ forms.py
@@ -1,13 +1,35 @@
 from flask_wtf import FlaskForm
+from flask_ckeditor import CKEditorField
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 
 # Form for adding new posts
 class AddEditPost(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     category = SelectField(u'Category', choices=[('cat1', 'Category 1'), ('cat2', 'Category 2'), ('cat3', 'Category 3'), ('cat4', 'Category 4')])
     draft = BooleanField('Draft')
-    body = TextAreaField('Content Area')
-    submit = SubmitField('Create Post')
+    body = CKEditorField('Content Area')
+    submit = SubmitField('Save Post')
 
-# class LoginForm(FlaskForm):
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    # def validate_username(self, username):
+    #     user = User.query.filter_by(username=username.data).first()
+    #     if user is not None:
+    #         raise ValidationError('Please use a different username.')
+    #
+    # def validate_email(self, email):
+    #     user = User.query.filter_by(email=email.data).first()
+    #     if user is not None:
+    #         raise ValidationError('Please use a different email address.')
